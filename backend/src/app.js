936c86e474a36b40665499ebc142ce2379e23f09
Kapil-dhakad <dhakadkapil350@gmail.com>
2026-02-03 00:25:38 +0530

    feat: add dotenv and cors for environment configuration and CORS handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
+
+// Middleware to handle cors
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173/",
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Athorization"]
+}))
+
+// Middleware to handle JSON object in req body
 
 export default app;
