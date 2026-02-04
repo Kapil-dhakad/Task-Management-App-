9d956502aafad2751654e07714021bcf3d92e778
Kapil-dhakad <dhakadkapil350@gmail.com>
2026-02-05 01:22:47 +0530

    feat: implement task model with schema definition and default values

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,51 @@
+import mongoose, { mongo } from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    priority: {
+        type: String,
+        enum: ["Low", "Medium", "high"],
+        default: "Low"
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "In Progress", "Completed"],
+        default: "Pending"
+    },
+    dueDate: {
+        type: String,
+        required: true
+    },
+    assignedTo: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+],
+    createdBy: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    attachments:[
+        {
+            type: String
+        }
+    ],
+    todoChecklist: [todoSchema],
+
+    progress: {type: Number, default: 0}
+
+},{timestamps: true}
+)
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
